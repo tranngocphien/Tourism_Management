f5aaeea2512ee90dd628c00a7a403cb7e70b0e59
tranhaitrung <anhtrung.ttn@gmail.com>
2021-05-14 00:18:34 +0700

    push head div

---
+++ app/views/includes/header.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Register Form</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/Tourism_Management/public/css/index.css">
+    <title>
+        <?php echo "hello"; ?>
+    </title>
+
+</head>
+
+<body>
+    <div class="header">
+        <div class="top">0204668833</div>
+        <div class="bot">
+            <div class="left">
+                <div class="logo"> 
+                    <img src="<?php echo URL ?>/public/img/sunlogo.png" class="logo">
+                </div>
+                <div class="name">
+                    SUNNY TOURIST
+                </div>
+            </div>
+            <div class="right">
+                <div class="home">
+                    TRANG CHỦ
+                </div>
+                <div class="infor">
+                    GIỚI THIỆU
+                </div>
+                <div class="contact">
+                    LIÊN HỆ
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <h1>this is title</h1>
+    </div>
+
+</body>
+
+</html>
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Register Form</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="http://localhost/Tourism_Management/public/css/style.css">
-
-    </head>
-    <body>
-        <div class="header">
-            <div>Tourism Management</div>
-        </div>
-    </body>
-</html>
+<?php
+ require_once ROOT . '/views/includes/header.php';
