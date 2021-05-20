f54e1b1478da624de0d4a0a6a78e036d2e458778
ghuioio <54397393+ghuioio@users.noreply.github.com>
2021-05-20 07:58:43 +0700

    Update register.php

--- app/views/users/register.php
+++ app/views/users/register.php
@@ -5,18 +5,20 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="<?php echo URL ?>/public/css/register.css">
-
+        
     </head>
     <body >
         <div class="login_form">
-            <form action="<?php echo URL ?>/users/login" method="post" name="registerForm">
-            <form action="<?php echo URL ?>/users/register" method="post" name="registerForm">
+            
+            
         <div class="login-wrap">
 	<div class="login-html">
 		<input id="tab-1" type="radio" name="tab" class="sign-in" checked><label for="tab-1" class="tab">Sign In</label>
 		<input id="tab-2" type="radio" name="tab" class="sign-up"><label for="tab-2" class="tab">Sign Up</label>
 		<div class="login-form">
+                    <form action="<?php echo URL ?>/users/login" method="post" name="login">
 			<div class="sign-in-htm">
+                            
 				<div class="group">
 					<label for="user" class="label">Username</label>
                                         <input id="user" type="text" class="input" name="username_login">
@@ -27,14 +29,17 @@
 				</div>
 				
 				<div class="group">
-					<input type="submit" class="button" value="Sign In" >
+                                    <input type="submit" class="button" value="Sign In" name="Sign In">
 				</div>
 				<div class="hr"></div>
 				<div class="foot-lnk">
 					<label for="tab-2">Doesn't have a account</a>
 				</div>
 			</div>
+                    </form>
+                    <form action="<?php echo URL ?>/users/register" method="post" name="register">
 			<div class="sign-up-htm">
+                            
 				<div class="group">
 					<label for="user" class="label">Username</label>
 					<input id="user" type="text" class="input" name="username">
@@ -60,13 +65,14 @@
                                         <input id="address" type="text" class="input" name="address">
 				</div>
 				<div class="group">
-					<input type="submit" class="button" value="Sign Up">
+                                    <input type="submit" class="button" value="Sign Up" name="Sign Up">
 				</div>
 				<div class="hr"></div>
 				<div class="foot-lnk">
 					<label for="tab-1">Already Member?</a>
 				</div>
 			</div>
+                    </form>        
 		</div>
 	</div>
 </div>
