bf8b05a7495321ce878533ca96da08616deeae1a
tranhaitrung <anhtrung.ttn@gmail.com>
2021-05-25 16:58:36 +0700

    fix login

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -1,15 +1,18 @@
 <?php
 
 
-class Users extends Controller {
+class Users extends Controller
+{
 
     private $userModel;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->userModel = $this->model('User');
     }
 
-    public function register() {
+    public function register()
+    {
         if (isset($_POST['username'])) {
             $username = $_POST['username'];
             $password = $_POST['password'];
@@ -19,39 +22,42 @@ class Users extends Controller {
             $address = $_POST['address'];
             $this->userModel->register($username, $password, $email, $phone, $fulname, $address);
             header("Location:" . URL . "/users/register");
-            
         } else {
             $this->view("users/register");
         }
     }
 
-    public function login() {
-        if (isset($_POST['username_login']) && $_POST['username_login']!='') {
+    public function login()
+    {
+        if (isset($_POST['username_login']) && $_POST['username_login'] != '') {
             $username = $_POST['username_login'];
             $password = $_POST['password_login'];
             $data = $this->userModel->login($username);
 
             // print_r($data);
             // die;
-            if( $username == "admin" && $password == "admin"){
+            if ($username == "admin" && $password == "admin") {
                 $_SESSION["admin"] = "admin";
-                header("Location:". URL . "/admins");
+                header("Location:" . URL . "/admins");
             }
             //print_r($data);
             //die ($data);
-            if ( $data[0]["User"]["password"] == $password) {
-                $_SESSION['username'] = $username;
-                $_SESSION['user_id'] = $data[0]["User"]["user_id"];
-                if (isset($_SESSION['link'])){
-                    $link = $_SESSION['link'];
-                    unset($_SESSION['link']);
-                    header("location:" . $link);
-                   
+            if (count($data) > 0) {
+                if ($data[0]["User"]["password"] == $password) {
+                    $_SESSION['username'] = $username;
+                    $_SESSION['user_id'] = $data[0]["User"]["user_id"];
+                    if (isset($_SESSION['link'])) {
+                        $link = $_SESSION['link'];
+                        unset($_SESSION['link']);
+                        header("location:" . $link);
+                    } else {
+                        $header = header("Location:" . URL . "/pages/index");
+                    }
                 }
                 else {
-                    $header = header("Location:" . URL . "/pages/index");
+                    $this->view("users/register");
                 }
-                
+
             } else {
                 $this->view("users/register");
             }
@@ -60,28 +66,31 @@ class Users extends Controller {
         }
     }
 
-    public function index() {
+    public function index()
+    {
         $this->view("users/index");
     }
 
-    public function logout(){
+    public function logout()
+    {
         unset($_SESSION['username']);
         header("Location:" . URL . "/");
     }
 
-    public function book($tour_id){
-        if (isset($_SESSION['username'])){
+    public function book($tour_id)
+    {
+        if (isset($_SESSION['username'])) {
             $username = $_SESSION['username'];
             $_SESSION['tour_id'] = $tour_id;
             $user = $this->userModel->getUserByUsername($username);
             $tour = $this->userModel->getTourById($tour_id);
-            $data = array (
+            $data = array(
                 "user" => $user,
                 "tour" => $tour
             );
 
             $this->view('users/book', $data);
-        }else {
+        } else {
             $_SESSION['link'] = "http://localhost/Tourism_Management/Users/book/$tour_id";
             $this->view('users/register');
 
@@ -89,13 +98,14 @@ class Users extends Controller {
         }
     }
 
-    public function carts(){
+    public function carts()
+    {
 
-        if (isset($_SESSION['username'])){
+        if (isset($_SESSION['username'])) {
             $user_id = $_SESSION['user_id'];
 
-            if(isset($_POST['date']) && isset($_POST['ticket']) && isset($_POST['thanhtoan'])){
-                
+            if (isset($_POST['date']) && isset($_POST['ticket']) && isset($_POST['thanhtoan'])) {
+
                 $tour_id = $_SESSION['tour_id'];
                 $ticket = $_POST['ticket'];
                 $status = "đang xác nhận";
@@ -105,9 +115,8 @@ class Users extends Controller {
 
                 $tour = $this->userModel->getTourById($tour_id);
                 $price = 0;
-                if ($ticket < 5 && $ticket > 0){
+                if ($ticket < 5 && $ticket > 0) {
                     $price = $tour[0]["Tour"]["price_personal"];
-                
                 } else if ($ticket >= 5) {
                     $price = $tour[0]["Tour"]["price_group"];
                 }
@@ -115,20 +124,16 @@ class Users extends Controller {
                 $total = $price * $ticket;
 
                 $this->userModel->book($user_id, $tour_id, $ticket, $status, $date_start, $date_booking, $total, $payment);
-                unset($_SESSION['tour_id']);       
-
+                unset($_SESSION['tour_id']);
             }
 
             $data = $this->userModel->getCarts($user_id);
 
-            $this->view('users/carts',$data);
-        }else {
-            $_SESSION['link']="http://localhost/Tourism_Management/Users/carts";
-           $this->view('users/register');
-           //print_r($_SESSION['link']);
+            $this->view('users/carts', $data);
+        } else {
+            $_SESSION['link'] = "http://localhost/Tourism_Management/Users/carts";
+            $this->view('users/register');
+            //print_r($_SESSION['link']);
         }
-
-
     }
-
 }
